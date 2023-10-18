d3eac58bff4e102f281179d7a815d697aafcfc11
gdavilaphoinike <g.davila@phoinikel.com>
2023-10-18 17:11:29 -0400

    feat/CFG-007: Creation of new truck type Modal and importations of modules for it to work.

--- src/app/pages/home/features/settings/settings.component.ts
+++ src/app/pages/home/features/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/core/services';
+import { NewTruckTypeComponent } from './components/new-truck-type/new-truck-type.component';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor( private modalService:ModalService ) { }
 
   ngOnInit(): void {
+  }
+
+  truckTypeModal() {
+
+    const dialogRef = this.modalService.openDialog(
+      NewTruckTypeComponent,
+      'Nuevo Tipo de Camión',
+      '700px',
+      '300px'
+    );
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('El diálogo camión ha sido cerrado');
+    });
+
   }
 
 }
--- src/app/pages/home/features/settings/settings.module.ts
+++ src/app/pages/home/features/settings/settings.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings.component';
 import { Routes, RouterModule } from '@angular/router';
 
+import { NewTruckTypeComponent } from './components/new-truck-type/new-truck-type.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { PhkInputModule } from 'src/app/shared/components/phk-input/phk-input.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
 const routes: Routes = [
   {
     path: '',
@@ -12,11 +18,15 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    SettingsComponent
+    SettingsComponent,
+    NewTruckTypeComponent
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MatButtonModule,
+    PhkInputModule,
+    ReactiveFormsModule
   ]
 })
 export class SettingsModule { }
