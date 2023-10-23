8a9f6576fed9596fc1dfd9278e44a75158189a07
JoseEAgreda <joseonix.16@gmail.com>
2023-10-23 18:08:49 -0400

    Refactor code

--- src/app/pages/fall-gelb/views/settings/settings.component.ts
+++ src/app/pages/fall-gelb/views/settings/settings.component.ts
@@ -1,9 +1,4 @@
-import { FormBankAccountComponent } from './bank-account/form-bank-account/form-bank-account.component';
-import { FormBankComponent } from './bank/form-bank/form-bank.component';
-import { ModalService } from './../../../../core/services/modal.service';
 import { Component, OnInit } from '@angular/core';
-import { FormUserComponent } from './user/form-user/form-user.component';
-import { FormCurrencyComponent } from './currency/form-currency/form-currency.component';
 
 @Component({
   selector: 'app-settings',
@@ -11,59 +6,59 @@ import { FormCurrencyComponent } from './currency/form-currency/form-currency.co
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
-  constructor(private modalService: ModalService) {}
+  constructor() {}
 
   ngOnInit(): void {}
 
-  showFormUser() {
-    const dialogRef = this.modalService.openDialog(
-      FormUserComponent,
-      'Registro de usuario',
-      '80%',
-      '90%'
-    );
+  // showFormUser() {
+  //   const dialogRef = this.modalService.openDialog(
+  //     FormUserComponent,
+  //     'Registro de usuario',
+  //     '80%',
+  //     '90%'
+  //   );
 
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log('El diálogo ha sido cerrado');
-    });
-  }
+  //   dialogRef.afterClosed().subscribe((result) => {
+  //     console.log('El diálogo ha sido cerrado');
+  //   });
+  // }
 
-  showFormBanck() {
-    const dialogRef = this.modalService.openDialog(
-      FormBankComponent,
-      'Registro de banco',
-      '80%',
-      '90%'
-    );
+  // showFormBanck() {
+  //   const dialogRef = this.modalService.openDialog(
+  //     FormBankComponent,
+  //     'Registro de banco',
+  //     '80%',
+  //     '90%'
+  //   );
 
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log('El diálogo ha sido cerrado');
-    });
-  }
+  //   dialogRef.afterClosed().subscribe((result) => {
+  //     console.log('El diálogo ha sido cerrado');
+  //   });
+  // }
 
-  showFormCurrency() {
-    const dialogRef = this.modalService.openDialog(
-      FormCurrencyComponent,
-      'Registro de banco',
-      '80%',
-      '90%'
-    );
+  // showFormCurrency() {
+  //   const dialogRef = this.modalService.openDialog(
+  //     FormCurrencyComponent,
+  //     'Registro de banco',
+  //     '80%',
+  //     '90%'
+  //   );
 
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log('El diálogo ha sido cerrado');
-    });
-  }
+  //   dialogRef.afterClosed().subscribe((result) => {
+  //     console.log('El diálogo ha sido cerrado');
+  //   });
+  // }
 
-  showFormBankAccount() {
-    const dialogRef = this.modalService.openDialog(
-      FormBankAccountComponent,
-      'Registro de Cuenta Bancaria',
-      '80%',
-      '90%'
-    );
+  // showFormBankAccount() {
+  //   const dialogRef = this.modalService.openDialog(
+  //     FormBankAccountComponent,
+  //     'Registro de Cuenta Bancaria',
+  //     '80%',
+  //     '90%'
+  //   );
 
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log('El diálogo ha sido cerrado');
-    });
-  }
+  //   dialogRef.afterClosed().subscribe((result) => {
+  //     console.log('El diálogo ha sido cerrado');
+  //   });
+  // }
 }
