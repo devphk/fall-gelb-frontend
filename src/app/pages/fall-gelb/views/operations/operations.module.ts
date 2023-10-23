72db7bd8f3bb7053b85e312d23e2ac7d44b8e2b8
JoseEAgreda <joseonix.16@gmail.com>
2023-10-23 13:39:17 -0400

    Refactor code

--- src/app/pages/fall-gelb/views/operations/operations.module.ts
+++ src/app/pages/fall-gelb/views/operations/operations.module.ts
@@ -3,6 +3,23 @@ import { CommonModule } from '@angular/common';
 import { OperationsComponent } from './operations.component';
 import { Routes, RouterModule } from '@angular/router';
 import { PhkTableModule } from 'src/app/shared/components/phk-table/phk-table.module';
+import {
+  PhkDatePickerModule,
+  PhkInputModule,
+  PhkSelectModule,
+  PhkSlideToggleModule,
+  PhkTextAreaModule,
+} from '@shared/components';
+import { CertificationComponent } from './views/certification/certification.component';
+import { FormCertificationComponent } from './views/components/form-certification/form-certification.component';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { EntitiesRoutingModule } from '../entities/entities-routing.module';
 
 const routes: Routes = [
   {
@@ -12,7 +29,28 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [OperationsComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), PhkTableModule],
+  declarations: [
+    OperationsComponent,
+    CertificationComponent,
+    FormCertificationComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    EntitiesRoutingModule,
+    PhkTableModule,
+    PhkDatePickerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    PhkInputModule,
+    PhkSelectModule,
+    MatOptionModule,
+    PhkTextAreaModule,
+    PhkSlideToggleModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    FlexLayoutModule,
+  ],
 })
 export class OperationsModule {}
