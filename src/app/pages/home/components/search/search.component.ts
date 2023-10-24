5223249bf1f198f1d0931f16ace23234ccd5f613
abinassar <abinassar@gmail.com>
2023-10-23 21:37:40 -0400

    feat/COR-003: Improve the logic of apps list and user account component

--- src/app/pages/home/components/search/search.component.ts
+++ src/app/pages/home/components/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { fadeAnimation, fadeFastAnimation } from '@shared/animations';
 import { PhkThemeToggleService } from '@shared/components';
 import { Mode } from '@shared/models';
+import { HomeService } from '../../home.service';
 
 @Component({
   selector: 'app-search',
@@ -36,16 +37,40 @@ export class SearchComponent implements OnInit {
                          .subscribe((mode: Mode) => {
     this.currentMode = mode;
   });
+  clickingAppsButton: boolean = false;
+  clickingUserButton: boolean = false;
 
-  constructor(private themeService: PhkThemeToggleService) { }
+  constructor(private themeService: PhkThemeToggleService,
+              public homeService: HomeService) { }
 
   ngOnInit(): void {
   }
 
   ngOnDestroy(): void {
-    //Called once, before the instance is destroyed.
-    //Add 'implements OnDestroy' to the class.
     this.themeSubscriptor.unsubscribe();
   }
 
+  toggleAppsList() {
+
+    this.clickingAppsButton = true;
+    this.homeService.toggleAppsList = !this.homeService.toggleAppsList;
+    this.homeService.toggleAppsList ? 
+    this.homeService.appsListOpened = true : 
+    this.homeService.appsListOpened = false;
+
+    setTimeout(() => {
+      this.clickingAppsButton = false;
+    }, 100);
+  }
+
+  toggleUserAccount() {
+    this.clickingUserButton = true;
+    this.homeService.toggleUserAccount = !this.homeService.toggleUserAccount;
+    this.homeService.toggleUserAccount ? 
+    this.homeService.userAccountOpened = true : 
+    this.homeService.userAccountOpened = false;
+    setTimeout(() => {
+      this.clickingUserButton = false;
+    }, 100);
+  }
 }
