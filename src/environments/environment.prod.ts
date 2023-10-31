9a9a2d0d124b5122a39aa7863fbd9a7521b3df52
abinassar <abinassar@gmail.com>
2023-10-31 06:51:04 -0400

    refactor: changes in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  urlBase: 'http://fallgelb-api.phoinikel.com/api',
+  oAuthClientId: '9a76c3ea-5f4a-4a34-a55b-52836c9c38a7',
+  oAuthClientToken: 'AqcgWvwnrIYd1fbFvJt6Mn1yvFiUMmHXQXjfALGY'
 };
