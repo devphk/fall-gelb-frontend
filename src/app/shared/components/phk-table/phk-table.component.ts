9d9895ee26a320dd3ddbafad6b76a2406f0c7adb
abinassar <abinassar@gmail.com>
2023-11-05 10:13:18 -0400

    refactor: improves in the table to get multiple values array

--- src/app/shared/components/phk-table/phk-table.component.ts
+++ src/app/shared/components/phk-table/phk-table.component.ts
@@ -1,19 +1,31 @@
 import { SelectionModel } from '@angular/cdk/collections';
-import { Component, EventEmitter, Input, OnInit, DoCheck, Output, ViewChild, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  DoCheck,
+  Output,
+  ViewChild,
+  ChangeDetectorRef,
+  ElementRef
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { fadeAnimation } from '../../animations';
+import { MatSelectChange } from '@angular/material/select';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-phk-table',
   templateUrl: './phk-table.component.html',
   styleUrls: ['./phk-table.component.scss'],
-  animations: [fadeAnimation]
+  animations: [fadeAnimation],
 })
-export class PhkTableComponent implements OnInit,
-                                          DoCheck {
+export class PhkTableComponent implements OnInit, DoCheck {
 
+  @ViewChild('multipleSelect') multipleSelect!: ElementRef;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
@@ -26,63 +38,55 @@ export class PhkTableComponent implements OnInit,
   @Input() showSelectColumn: boolean = true;
 
   // Two binding of items selected
-  
+
   @Input() itemsSelected: any[] = [];
   @Output() itemsSelectedChange = new EventEmitter<any[]>();
-    
+
   dataSource = new MatTableDataSource<any>();
   selection = new SelectionModel<any>(true, []);
   backupData: any[] = [];
   showSkeleton: boolean = true;
 
-  constructor(private changeDetectorRef: ChangeDetectorRef) { }
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
-
     if (this.showSelectColumn) {
-      this.columnsTags.unshift("select");
-      this.columnsToDisplay.unshift("select");
+      this.columnsTags.unshift('select');
+      this.columnsToDisplay.unshift('select');
     }
-
 
     this.setData();
   }
 
   ngDoCheck(): void {
-    
     if (this.backupData.length !== this.data.length) {
       this.changeDetectorRef.detectChanges();
       this.setData();
     }
-
   }
 
   setData() {
-
     this.showSkeleton = true;
-    let randomSeconds = Math.random()*2000;
+    let randomSeconds = Math.random() * 2000;
 
     setTimeout(() => {
-      
       this.dataSource = new MatTableDataSource<any>(this.data);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
       this.backupData = this.data.slice();
       // this.dataSource.filterPredicate = this.createFilter();
-  
+
       this.showSkeleton = false;
-
     }, randomSeconds);
-
   }
 
   // Selection logic
 
   /** Selects all rows if they are not all selected; otherwise clear selection. */
   masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSource.data.forEach(row => this.selection.select(row));
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach((row) => this.selection.select(row));
     this.itemsSelected.splice(0);
 
     if (this.selection.selected.length > 0) {
@@ -90,24 +94,55 @@ export class PhkTableComponent implements OnInit,
         this.itemsSelected.push(item);
       });
     }
+    console.log("selection ", this.selection)
+  }
+
+  updateCheckedList(event: any, element: any, rowIndex: number) {
+    console.log("event ", event)
+    console.log("element ", element)
+    console.log("rowIndex ", rowIndex)
+
+    
+    if (event.checked) {
+
+      this.itemsSelected.push(element);
+
+    } else {
+
+      let index = this.itemsSelected.findIndex(
+        (item) => item.UserName === element.UserName
+      );
+      if (index !== -1) {
+        this.itemsSelected.splice(index, 1);
+      }
+
+    }
 
   }
 
-  updateCheckedList(event: any, element: any) {
-    console.log("event ", event)
-    console.log("Element selected ", element)
-    if (event.checked) {
-      this.itemsSelected.push(element);
-    } else {
-      let i = 0;
-      this.itemsSelected.forEach((item: any) => {
-        if (item.UserName === element.UserName) {
-          this.itemsSelected.splice(i, 1);
-          return;
-        }
-        i++;
-      });
+  selectRow(checkboxChange: MatCheckboxChange,
+            rowElement: any,
+            rowIndex: number) {
+
+    if (checkboxChange) {
+
+      this.selection.toggle(rowElement);
+      this.updateCheckedList(checkboxChange, rowElement, rowIndex);
+
+      // I check the first option of multiselect options
+
+      if (this.data[rowIndex].options) {
+
+        let itemSelectedIndex = this.itemsSelected.findIndex((item) => {
+          return item.rowIndex === rowIndex
+        })
+        this.itemsSelected[itemSelectedIndex]
+            .optionsSelected
+            .push(this.itemsSelected[itemSelectedIndex].options[0]);
+
+      }
     }
+              
   }
 
   /** Whether the number of selected elements matches the total number of rows. */
@@ -118,6 +153,45 @@ export class PhkTableComponent implements OnInit,
   }
 
   getType(variable: any) {
-    return typeof variable;
+
+    if (Array.isArray(variable)) {
+      return 'array';
+    } else {
+      return typeof variable;
+    }
+
   }
+
+  selectionChange(rowElement: any, selectEvent: MatSelectChange, rowIndex: number) {
+    console.log("rowIndex ", rowIndex)
+    console.log("event ", selectEvent)
+    console.log("this.data ", this.data)
+
+    let event = {
+      checked: selectEvent.value.length > 0 ? true : false
+    }
+
+    // First i check if the item exist in the selected
+    // Items attay
+
+    let existIndex = this.itemsSelected.findIndex((item) => {
+      return item.rowIndex === rowIndex
+    });
+
+    if (selectEvent.value.length === 0
+        || existIndex === -1) {
+      this.selection.toggle(rowElement);
+      this.updateCheckedList(event, this.data[rowIndex], rowIndex);
+    }
+
+    let itemIndex = this.itemsSelected.findIndex((item) => {
+      return item.rowIndex === rowIndex
+    });
+
+    if (itemIndex !== -1) {
+      this.itemsSelected[itemIndex].optionsSelected = selectEvent.value;
+    }
+
+  }
+
 }
