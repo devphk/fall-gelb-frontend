2e230fec9ce6facaac59e835338868f2e0a69fbc
gdavilaphoinike <g.davila@phoinikel.com>
2023-11-07 11:11:47 -0400

    feat/CFG-002: Changes in the code of Customs.

--- src/app/pages/fall-gelb/views/settings/views/components/new-customs/new-customs.component.ts
+++ src/app/pages/fall-gelb/views/settings/views/components/new-customs/new-customs.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { CustomsService } from '../../customs/customs.service';
 import { Transporttype } from '@shared/models';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ToastService } from '@core/services';
 
 @Component({
   selector: 'app-new-customs',
@@ -14,10 +15,10 @@ export class NewCustomsComponent implements OnInit{
 
   customsForm: FormGroup = this.fb.group({
     name: this.fb.control(this.data.dialogData ? this.data.dialogData[0].name : '', [Validators.required]),
-    address: this.fb.control(this.data.dialogData ? this.data.dialogData[0].address : '', [Validators.required]),
+    address: this.fb.control(this.data.dialogData ? this.data.dialogData[0].address : '',),
     transport_types: this.fb.control(""),
-    longitude: this.fb.control(this.data.dialogData ? this.data.dialogData[0].longitude : '', [Validators.required]),
-    latitude: this.fb.control(this.data.dialogData ? this.data.dialogData[0].latitude : '', [Validators.required])
+    longitude: this.fb.control(this.data.dialogData ? this.data.dialogData[0].longitude : '',),
+    latitude: this.fb.control(this.data.dialogData ? this.data.dialogData[0].latitude : '',)
   })
   transportType: Transporttype[] = [];
   selectedTypes: Transporttype[] = [];
@@ -29,7 +30,8 @@ export class NewCustomsComponent implements OnInit{
   constructor( private fb:FormBuilder,
                private customsService:CustomsService,
                private dialogRef:MatDialogRef<NewCustomsComponent>,
-               @Inject(MAT_DIALOG_DATA) private data: any) {}
+               @Inject(MAT_DIALOG_DATA) private data: any,
+               private toastService:ToastService) {}
 
   
   ngOnInit(): void {
@@ -73,36 +75,46 @@ export class NewCustomsComponent implements OnInit{
   }
 
   saveCustoms() {
-    if (this.customsForm.valid && this.data.title === 'Crear Aduana') {
-      const custom = {
-        name: this.customsForm.get('name')?.value, 
-        address: this.customsForm.get('address')?.value, 
-        latitude: this.customsForm.get('latitude')?.value,
-        longitude: this.customsForm.get('longitude')?.value,
-        transport_types: this.customForm.value,
-        // passwordVerify: this.customsForm.get('passwordVerify')?.value,
-        // role: this.customsForm.get('role')?.value  
+    if (this.customsForm.valid) {
+      if(this.data.title === 'Crear Aduana'){
+
+        const custom = {
+          name: this.customsForm.get('name')?.value, 
+          address: this.customsForm.get('address')?.value, 
+          latitude: this.customsForm.get('latitude')?.value,
+          longitude: this.customsForm.get('longitude')?.value,
+          transport_types: this.customForm.value,  
+        }
+  
+        this.customsService.createCustom(custom)
+          .subscribe((data) => {
+            this.toastService.showToaster("Aduana Creada Correctamente!")
+            this.dialogRef.close(true);
+          },
+                     (error) => console.error('ERROR! :', error))
+
+      }else{
+
+        const customEdit = {
+          name: this.customsForm.get('name')?.value, 
+          address: this.customsForm.get('address')?.value, 
+          latitude: this.customsForm.get('latitude')?.value,
+          longitude: this.customsForm.get('longitude')?.value,
+          transport_types: this.customForm.value,
+   
+        }
+  
+        this.customsService.editCustom(customEdit, this.data.dialogData[0].id)
+          .subscribe((data) => {
+            this.toastService.showToaster("Aduana Editada Correctamente!")
+            this.dialogRef.close(true);
+          },
+                     (error) => this.toastService.showToaster(error.error.message, true))
+
       }
 
-      this.customsService.postCustoms(custom)
-        .subscribe((data) => console.log('EXITOSO!: ', data),
-                   (error) => console.error('ERROR! :', error))
-
-    } else if(this.customsForm.valid && this.data.title === 'Editar Aduana'){
-      // this.customsForm.markAllAsTouched();
-      const customEdit = {
-        name: this.customsForm.get('name')?.value, 
-        address: this.customsForm.get('address')?.value, 
-        latitude: this.customsForm.get('latitude')?.value,
-        longitude: this.customsForm.get('longitude')?.value,
-        transport_types: this.customForm.value,
-        // passwordVerify: this.customsForm.get('passwordVerify')?.value,
-        // role: this.customsForm.get('role')?.value  
-      }
-
-      this.customsService.putCustom(customEdit, this.data.dialogData[0].id)
-        .subscribe((data) => console.log('EXITOSO!: ', data),
-                   (error) => console.error('ERROR! :', error))
+    } else{
+      this.customForm.markAllAsTouched();
 
     }
   }
--- src/app/shared/models/loading-messages.enum.ts
+++ src/app/shared/models/loading-messages.enum.ts
@@ -1,6 +1,8 @@
 export enum LoadingMessage {
   DEFAULT = 'Cargando...',
   CREATING_ROLE = 'Creando rol...',
+  CREATING_USER = 'Creando usuario...',
   DELETING_USER = 'Eliminando usuario...',
-  CREATING_USER = 'Creando usuario...'
+  CREATING_CUSTOM = 'Creando aduana...',
+  DELETING_CUSTOM = 'Eliminando aduana...'
 }
