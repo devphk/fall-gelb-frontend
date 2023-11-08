de14d4b54365f9556cb485aef89696ed7cf678e4
gdavilaphoinike <g.davila@phoinikel.com>
2023-11-08 08:45:24 -0400

    feat/CFG-002: Added maps view in the customs modal.

--- src/app/pages/fall-gelb/views/settings/views/components/new-customs/new-customs.component.ts
+++ src/app/pages/fall-gelb/views/settings/views/components/new-customs/new-customs.component.ts
@@ -4,6 +4,8 @@ import { CustomsService } from '../../customs/customs.service';
 import { Transporttype } from '@shared/models';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ToastService } from '@core/services';
+import { Map, Marker, marker, popup, tileLayer } from 'leaflet';
+import { DialogData } from '../../../../../../../core/models/dialog';
 
 @Component({
   selector: 'app-new-customs',
@@ -17,11 +19,13 @@ export class NewCustomsComponent implements OnInit{
     name: this.fb.control(this.data.dialogData ? this.data.dialogData[0].name : '', [Validators.required]),
     address: this.fb.control(this.data.dialogData ? this.data.dialogData[0].address : '',),
     transport_types: this.fb.control(""),
-    longitude: this.fb.control(this.data.dialogData ? this.data.dialogData[0].longitude : '',),
-    latitude: this.fb.control(this.data.dialogData ? this.data.dialogData[0].latitude : '',)
   })
   transportType: Transporttype[] = [];
   selectedTypes: Transporttype[] = [];
+  mapLongitude: number = 0;
+  mapLatitude: number = 0;
+  mapActualLongitude: number = 0;
+  mapActualLatitude: number = 0;
 
   customForm: FormControl = new FormControl();
 
@@ -37,6 +41,8 @@ export class NewCustomsComponent implements OnInit{
   ngOnInit(): void {
     this.getTranspTypes();
     this.getSelectedTransType();
+    this.getCurrentLocation();
+    console.log('DATA: ', this.data);
   }
 
   getTranspTypes() {
@@ -62,7 +68,7 @@ export class NewCustomsComponent implements OnInit{
 
   getSelectedTransType() {
     const transportTypeIds = [];
-
+    
     if (this.data && this.data.dialogData && this.data.dialogData[0].transport_types) {
       for (let i = 0; i < this.data.dialogData[0].transport_types.length; i++) {
           transportTypeIds.push(this.data.dialogData[0].transport_types[i].id);
@@ -74,6 +80,82 @@ export class NewCustomsComponent implements OnInit{
     this.customForm = new FormControl(this.selectedTypes);
   }
 
+    getCurrentLocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.mapActualLatitude = position.coords.latitude;
+          this.mapActualLongitude = position.coords.longitude;
+          console.log(this.mapActualLatitude)
+          console.log(this.mapActualLongitude)
+          this.showMapWithMarker();
+        },
+        (error) => {
+          console.error('Error getting current location:', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }
+
+  showMapWithMarker(): void {
+    if(this.data.dialogData === null) {
+      
+      console.log('Latitude',this.mapActualLatitude);
+      console.log('Longitude', this.mapActualLongitude);
+      const map = new Map('map').setView([this.mapActualLatitude, this.mapActualLongitude], 13);
+      tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+    
+      const marker = new Marker([this.mapActualLatitude, this.mapActualLongitude]).addTo(map);
+
+    }else{
+
+      console.log('Latitude', this.data.dialogData[0].latitude);
+      console.log('Longitude', this.data.dialogData[0].longitude);
+      const map = new Map('map').setView([this.data.dialogData[0].latitude, this.data.dialogData[0].longitude], 8);
+      tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+    
+      const marker = new Marker([this.data.dialogData[0].latitude, this.data.dialogData[0].longitude]).addTo(map);
+
+    }
+    
+  }
+  
+  //Click on map to change location code
+  // ngAfterViewInit(): void {
+
+  //   const map = new Map('map').setView([7.721123907246407, -65.67882751949143], 5);
+  //   tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //     maxZoom: 19,
+  //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  //   }).addTo(map);
+
+  //   let previousMarker: Marker | null = null;
+
+  //   map.on('click', (event) => {
+  //     const latLng = event.latlng;
+  //     this.mapLatitude = latLng.lat;
+  //     this.mapLongitude = latLng.lng;
+  //     console.log('Latitud:', this.mapLatitude, 'Longitud:', this.mapLongitude);
+
+  //     if(previousMarker) {
+  //       previousMarker.remove();
+  //     }
+
+  //     const newMarker = marker([this.mapLatitude, this.mapLongitude]).addTo(map);
+
+  //     previousMarker = newMarker;
+      
+  //   });
+  // }
+  
   saveCustoms() {
     if (this.customsForm.valid) {
       if(this.data.title === 'Crear Aduana'){
@@ -81,11 +163,11 @@ export class NewCustomsComponent implements OnInit{
         const custom = {
           name: this.customsForm.get('name')?.value, 
           address: this.customsForm.get('address')?.value, 
-          latitude: this.customsForm.get('latitude')?.value,
-          longitude: this.customsForm.get('longitude')?.value,
+          latitude: this.mapActualLatitude,
+          longitude: this.mapActualLongitude,
           transport_types: this.customForm.value,  
         }
-  
+          console.log('Custom: ', custom)
         this.customsService.createCustom(custom)
           .subscribe((data) => {
             this.toastService.showToaster("Aduana Creada Correctamente!")
@@ -98,8 +180,8 @@ export class NewCustomsComponent implements OnInit{
         const customEdit = {
           name: this.customsForm.get('name')?.value, 
           address: this.customsForm.get('address')?.value, 
-          latitude: this.customsForm.get('latitude')?.value,
-          longitude: this.customsForm.get('longitude')?.value,
+          latitude: this.data.DialogData[0].latitude,
+          longitude: this.data.DialogData[0].longitude,
           transport_types: this.customForm.value,
    
         }
@@ -118,6 +200,11 @@ export class NewCustomsComponent implements OnInit{
 
     }
   }
+
+
+
+
+
 }
 
 
