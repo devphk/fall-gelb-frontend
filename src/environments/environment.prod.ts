1f83597e4a8c2e554dc5dcf5b655216f670d7d71
abinassar <abinassar@gmail.com>
2023-11-14 23:03:15 -0400

    refactor: changed the url oauth environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  urlBase: 'http://fallgelb-api.phoinikel.com/api',
+  urlBase: 'https://fallgelb-api.phoinikel.com/api',
+  urlOAuth: 'https://fallgelb-api.phoinikel.com/oauth/token',
   oAuthClientId: '9a76c3ea-5f4a-4a34-a55b-52836c9c38a7',
   oAuthClientToken: 'AqcgWvwnrIYd1fbFvJt6Mn1yvFiUMmHXQXjfALGY'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   urlBase: 'https://fallgelb-api.phoinikel.com/api',
+  urlOAuth: 'https://fallgelb-api.phoinikel.com/oauth/token',
   oAuthClientId: '9a9276f5-9274-4afd-9830-b1ca9c54c1b8',
   oAuthClientToken: 'PJoziPOCifAc9ewn2q9PpMCjcfDA8bTlhpP0lDIf'
 };
