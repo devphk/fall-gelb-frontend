d2919cc87afebfc27551b9b9a06c386fcbc51de7
abinassar <abinassar@gmail.com>
2023-11-14 22:58:16 -0400

    refactor: changed the url base environment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  urlBase: 'http://fallgelb-api.phoinikel.com/api',
+  urlBase: 'https://fallgelb-api.phoinikel.com/api',
   oAuthClientId: '9a9276f5-9274-4afd-9830-b1ca9c54c1b8',
   oAuthClientToken: 'PJoziPOCifAc9ewn2q9PpMCjcfDA8bTlhpP0lDIf'
 };
