def4995623d5cecb637f15ad0d169eeb3e06567c
abinassar <abinassar@gmail.com>
2023-11-20 19:44:48 -0400

    refactor: changes in the environment variables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
         path: 'sign', 
         loadChildren: () => import('./pages/sign/sign.module').then(m => m.SignModule)
     },
-    { 
+    {
         path: 'home', 
         loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
     },
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   urlBase: 'https://fallgelb-api.phoinikel.com/api',
   urlOAuth: 'https://fallgelb-api.phoinikel.com/oauth/token',
-  oAuthClientId: '9a76c3ea-5f4a-4a34-a55b-52836c9c38a7',
-  oAuthClientToken: 'AqcgWvwnrIYd1fbFvJt6Mn1yvFiUMmHXQXjfALGY'
+  oAuthClientId: '9a9276f5-9274-4afd-9830-b1ca9c54c1b8',
+  oAuthClientToken: 'PJoziPOCifAc9ewn2q9PpMCjcfDA8bTlhpP0lDIf'
 };
