ef208440a46c1abf8dc8cf69a3ba15d51550fa8f
JoseEAgreda <joseonix.16@gmail.com>
2023-11-21 10:30:10 -0400

    fix in update driver

--- src/app/shared/models/driver.ts
+++ src/app/shared/models/driver.ts
@@ -5,7 +5,7 @@ export interface DriverDataPost {
   active: boolean;
   address: string;
   is_self_employed: boolean;
-  provider_id: number;
+  provider_id?: number | null;
 }
 export interface DriverDataTable {
   id: number;
@@ -18,4 +18,21 @@ export interface DriverDataTable {
   provider_id: number;
 }
 
-export interface DriverResponse {}
+export interface DriverResponse {
+  id: number;
+  entity_id: number;
+  is_self_employed: boolean;
+  provider_id: number;
+  entity: Entity;
+}
+
+interface Entity {
+  id: number;
+  name: string;
+  phone: string;
+  email: string;
+  active: boolean;
+  address: string;
+  created_at: string;
+  updated_at: string;
+}
