9503afb6b1171ee08d331650207417603385608b
abinassar <abinassar@gmail.com>
2023-11-24 17:31:45 -0400

    refactor: changed key in the prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   urlBase: 'https://fallgelb-api.phoinikel.com/api',
   urlOAuth: 'https://fallgelb-api.phoinikel.com/oauth/token',
   oAuthClientId: '9ab10c4d-9d3c-4e7b-b544-ac6ca106cb05',
-  oAuthClientToken: 'guCkj35gDWdyyQZu930wYCP3rT6eGsKqZks26gH8k'
+  oAuthClientToken: 'guCkj35gDWdyyQZu930wYCP3rT6eGsKqZks26gH8'
 };
