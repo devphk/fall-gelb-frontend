a5d478684facfbad8a518c01aae39bcfa8c8d30b
abinassar <abinassar@gmail.com>
2023-11-24 12:01:47 -0400

    feat/COR-016: Created component to browse and drag and drop documents

--- src/app/shared/components/phk-file-input/phk-file-input.component.ts
+++ src/app/shared/components/phk-file-input/phk-file-input.component.ts
@@ -9,6 +9,7 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { DragAndDropFilesModule } from '@shared/directives/drag-and-drop-files/drag-and-drop-files.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-phk-file-input',
@@ -17,7 +18,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     CommonModule,
     MatIconModule,
     DragAndDropFilesModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatTooltipModule
   ],
   templateUrl: './phk-file-input.component.html',
   styleUrls: ['./phk-file-input.component.scss']
@@ -28,8 +30,12 @@ export class PhkFileInputComponent implements OnInit {
 
   //Inputs
 
-  @Input() inputFile!: File;
-  @Output() inputFileChange = new EventEmitter<File>();
+  @Input() inputFile: File | undefined = undefined;
+  @Output() inputFileChange = new EventEmitter<File | undefined>();
+
+  fileOver: boolean = false;
+  fileName: string | undefined = '';
+  dragFile: boolean = false;
 
   constructor() { }
 
@@ -37,11 +43,24 @@ export class PhkFileInputComponent implements OnInit {
   }
 
   isMouseOver(mouseOver: boolean) {
-    console.log("mouseOver ", mouseOver)
+
+    if (this.fileOver !== mouseOver) {
+      this.fileOver = mouseOver;
+      console.log("this.fileOver ", this.fileOver)
+    }
+
+    if (this.fileOver && !this.inputFile) {
+      this.dragFile = true;
+    } else {
+      this.dragFile = false;
+    }
+
   }
 
   fileDropped(files: File[]) {
-    console.log("files ", files)
+    this.inputFile = files[0];
+    this.inputFileChange.emit(this.inputFile);
+    this.fileName = this.inputFile.name;
   }
 
   onFileSelected(event: any) {
@@ -49,6 +68,7 @@ export class PhkFileInputComponent implements OnInit {
     if (event.target.files) {
       this.inputFile = event.target.files[0];
       this.inputFileChange.emit(this.inputFile);
+      this.fileName = this.inputFile?.name;
     }
 
   }
@@ -57,4 +77,10 @@ export class PhkFileInputComponent implements OnInit {
     const fileInput = this.fileInput.nativeElement;
     fileInput.click();
   }
+
+  deleteFile() {
+    this.inputFile = undefined;
+    this.inputFileChange.emit(this.inputFile);  
+  }
+
 }
